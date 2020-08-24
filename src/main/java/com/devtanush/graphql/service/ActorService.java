e66c1f0e523cd2c6319b556b18fb183cd3cb3654
Nicholas Tanui <nicholastanui@gmail.com>
2020-08-24 17:05:17 +0300

    add subscription impl

--- src/main/java/com/devtanush/graphql/service/ActorService.java
+++ src/main/java/com/devtanush/graphql/service/ActorService.java
@@ -2,16 +2,21 @@ package com.devtanush.graphql.service;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;
 import com.devtanush.graphql.model.Actor;
 import com.devtanush.graphql.model.AddressInput;
 import com.devtanush.graphql.repository.ActorRepository;
+import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
-public class ActorService implements GraphQLQueryResolver, GraphQLMutationResolver {
+public class ActorService implements GraphQLQueryResolver, GraphQLMutationResolver, GraphQLSubscriptionResolver {
     // all public methods under classes implementing GraphQLQueryResolver will be registered as Query
     // and will try to map the name of the method with the ones under the schema file.
     private ActorRepository actorRepository;
@@ -19,6 +24,8 @@ public class ActorService implements GraphQLQueryResolver, GraphQLMutationResolv
     public ActorService(ActorRepository actorRepository) {
         this.actorRepository = actorRepository;
     }
+
+    private ConcurrentHashMap<Integer, FluxSink<Actor>> subscribers = new ConcurrentHashMap<>();
 
     public List<Actor> getAllActors() {
         return actorRepository.findAll();
@@ -33,6 +40,9 @@ public class ActorService implements GraphQLQueryResolver, GraphQLMutationResolv
         Actor actor = actorRepository.findById(id).get();
         actor.setAddress(address);
         actorRepository.save(actor);
+        if (subscribers.get(id) != null) {
+            subscribers.get(id).next(actor);
+        }
         return actor;
     }
 
@@ -41,6 +51,13 @@ public class ActorService implements GraphQLQueryResolver, GraphQLMutationResolv
         Actor actor = actorRepository.findById(input.getActorId()).get();
         actor.setAddress(input.getAddress());
         actorRepository.save(actor);
+        if (subscribers.get(input.getActorId()) != null) {
+            subscribers.get(input.getActorId()).next(actor);
+        }
         return actor;
+    }
+
+    public Publisher<Actor> onActorUpdate(Integer actorId) {
+        return Flux.create(subscriber -> subscribers.put(actorId, subscriber.onDispose(()-> subscribers.remove(actorId, subscriber))), FluxSink.OverflowStrategy.LATEST);
     }
 }
